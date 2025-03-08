7bdde520839a85cfbb181b3e24f20058d73fdc79
Bianca Alves Silva <142455130+biancaalves27@users.noreply.github.com>
2025-03-08 15:23:01 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('form-sorteador').addEventListener('submit', function(evento) {
         evento.preventDefault();
+
         let numeroMaximo = document.getElementById('numero-maximo').value;
-        numeroMaximo = parseInt(numeroMaximo);
+        numeroMaximo = parseInt(numeroMaximo); // Converte para número inteiro
 
+        // Verifica se o valor inserido é válido e dentro do intervalo
+        if (isNaN(numeroMaximo) || numeroMaximo < 2 || numeroMaximo > 600000) {
+            alert('Por favor, insira um número válido entre 2 e 600.000.');
+            return;
+        }
+
+        // Sorteio do número aleatório
         let numeroAleatorio = Math.random() * numeroMaximo;
-        numeroAleatorio = Math.floor(numeroAleatorio + 1);
+        numeroAleatorio = Math.floor(numeroAleatorio) + 1;
 
+        // Exibe o número sorteado
         document.getElementById('resultado-valor').innerText = numeroAleatorio;
         document.querySelector('.resultado').style.display = 'block';
     })
